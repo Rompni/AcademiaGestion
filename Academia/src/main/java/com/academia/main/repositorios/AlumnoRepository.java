b90a7bffb5ceefecc1b327272936d375ba5f6de7
Rompni <pipenavarrox@gmail.com>
2019-09-25 03:10:27 -0500

    se agregó el @Repository a todas interfaces del paquete repositorios

--- Academia/src/main/java/com/academia/main/repositorios/AlumnoRepository.java
+++ Academia/src/main/java/com/academia/main/repositorios/AlumnoRepository.java
@@ -1,9 +1,10 @@
 package com.academia.main.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.academia.main.entidades.Alumno;
-
+@Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
 
 }
--- Academia/src/main/java/com/academia/main/repositorios/ClaseRepository.java
+++ Academia/src/main/java/com/academia/main/repositorios/ClaseRepository.java
@@ -1,9 +1,10 @@
 package com.academia.main.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.academia.main.entidades.Clase;
-
+@Repository
 public interface ClaseRepository extends JpaRepository<Clase, Long> {
 
 }
