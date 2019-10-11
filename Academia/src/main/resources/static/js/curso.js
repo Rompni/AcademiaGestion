c5f239a745ec895c991b8993b7e0b2121bd84ca3
rompni <pipenavarrox@gmail.com>
2019-10-11 15:19:37 -0500

    Vistas
    Todas las vistas con algunas emergentes

--- Academia/src/main/resources/static/js/curso.js
+++ Academia/src/main/resources/static/js/curso.js
@@ -8,19 +8,4 @@ $('#myModal3').on('show.bs.modal', function (event) {
     modal.find('.modal-title').text(recipient)
     modal.find('.modal-footer button[id=boton]').text(recipient2)
   })
-
-$('#limpiar').on('click', function(event){
-    $('.card-body input').val("")
-  })
-
-window.onload = function(){
-    var fecha = new Date(); //Fecha actual
-    var mes = fecha.getMonth()+1; //obteniendo mes
-    var dia = fecha.getDate(); //obteniendo dia
-    var ano = fecha.getFullYear(); //obteniendo año
-    if(dia<10)
-      dia='0'+dia; //agrega cero si el menor de 10
-    if(mes<10)
-      mes='0'+mes //agrega cero si el menor de 10
-    document.getElementById('InputFechaAlta').value=ano+"-"+mes+"-"+dia;
-  }
+  
