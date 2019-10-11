ea9e1e34a62e03c5722b1151b0d7222bb3ab302c
rompni <pipenavarrox@gmail.com>
2019-10-11 00:55:55 -0500

    thyme

--- Academia/src/main/java/com/academia/main/AcademiaApplication.java
+++ Academia/src/main/java/com/academia/main/AcademiaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcademiaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaApplication.class, args);
 	}
