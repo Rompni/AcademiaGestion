c5ab800eebc26c38f2fab67db3e9dad384b5cc12
rompni <pipenavarrox@gmail.com>
2019-10-11 01:29:35 -0500

    x

--- Academia/src/main/java/com/academia/main/AcademiaApplication.java
+++ Academia/src/main/java/com/academia/main/AcademiaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcademiaApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaApplication.class, args);
 	}
